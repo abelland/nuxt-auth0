063e405baf07e81a2b236a1d95ab63aa5689e6de
Alessandro Bellanda <a.bellanda@gmail.com>
2020-04-19 15:30:12 +0200

    refactor isAuthenticated.js

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -5,10 +5,9 @@ export default async function({ route, app, redirect }) {
       (appState && appState.targetUrl) || process.env.AUTH0_CALLBACK_ROUTE
     )
   } catch (e) {
-    await app.$auth0.getTokenSilently()
-  } finally {
-    const isAuthenticated = await app.$auth0.isAuthenticated()
-    if (!isAuthenticated) {
+    try {
+      await app.$auth0.getTokenSilently()
+    } catch (e) {
       const url = await app.$auth0.buildAuthorizeUrl({
         appState: { targetUrl: route.fullPath },
         redirect_uri: window.location.origin + process.env.AUTH0_CALLBACK_ROUTE
